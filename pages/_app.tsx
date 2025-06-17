49375432d82a1b1831756d4e48347227b767f820
Sehajpreet Singh <sehajmagan@gmail.com>
2025-06-16 20:23:57 -0400

    added banner component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Roboto_Slab} from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const robotoSlab400 = Roboto_Slab({ subsets: ['latin'],weight: ['400'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={robotoSlab400.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
